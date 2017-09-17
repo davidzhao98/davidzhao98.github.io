a8259fcf84a737782df56e668fec39b258dad1ed
David Zhao <dzhao64@gatech.edu>
2017-09-17 04:35:32 -0400

    added js

---
+++ js/main.js
@@ -0,0 +1,42 @@
+$(function(){
+    $("#home").typed({
+        strings: ["DAVID ZHAO / "],
+        typeSpeed: 50,
+        startDelay: 250
+    });
+});
+$(function(){
+    $("#about-me").typed({
+        strings: ["ABOUT ME"],
+        typeSpeed: 50,
+        startDelay: 250
+    });
+});
+$(function(){
+    $("#projects").typed({
+        strings: ["PROJECTS"],
+        typeSpeed: 50,
+        startDelay: 250
+    });
+});
+$(function(){
+    $("#resume").typed({
+        strings: ["RESUME"],
+        typeSpeed: 50,
+        startDelay: 250
+    });
+});
+$(function(){
+    $("#contact").typed({
+        strings: ["CONTACT"],
+        typeSpeed: 50,
+        startDelay: 250
+    });
+});
+//
+// $("#backtotop").click(function() {
+//     return $("html,body").animate( {
+//         scrollTop: 0
+//     }
+//     , 2e3), !1
+// });
